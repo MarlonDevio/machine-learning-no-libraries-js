b4e2a0f330c260002fa2e70b3c5e4dd618cf1edb
marlonair <marlonair@laptopvarlonair.home>
2024-04-13 01:59:11 +0200

    rounding lines

--- js/draw.js
+++ js/draw.js
@@ -8,6 +8,9 @@ draw.path = (ctx, path, color = 'black') => {
   for (let i = 1; i < path.length; i++) {
     ctx.lineTo(...path[i]);
   }
+  ctx.lineCap = 'round';
+  ctx.lineJoin = 'round';
+
   ctx.stroke();
 };
 
